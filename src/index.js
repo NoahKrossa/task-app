ead73f4b2ad23594043dc6758cccbf864ea0236e
noah krossa <noahkrossa@gmail.com>
2020-03-02 16:05:42 +0100

    Enable Cors for front-end development server

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(morgan('dev'))
 
+// Enable CORS for front development
+app.use((req, res, next) => {
+        res. header("Access-Control-Allow-Origin", "*");
+        next()
+})
+
 // Routing
 app.use('/api', require('./router'))
 
