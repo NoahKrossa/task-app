ee2829a0736a4fda008749ac2e52cc2300230000
Noah Krossa <noahkrossa@gmail.com>
2020-10-20 17:38:27 +0200

    Add id to todo model

---
+++ src/todo.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const todoSchema = new Schema({
+    title: {type: String, required: true},
+    id: {type:String},
+    description: { type: String },
+    completed: {type: Boolean, default: false}
+})
+
+
+todoSchema.pre('save', function(done) {
+    const todo = this
+    todo.id = require('uuid').v4()
+    done()
+})
+
+module.exports = mongoose.model('todo', todoSchema)
